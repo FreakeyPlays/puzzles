edc40e77eaec2d133f666d2b4fb30d1a958a91ff
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-06-14 15:22:06 +0200

    feat(sudoku-web): include git commit hash in dev version string

--- apps/sudoku-web/src/environments/environment.development.ts
+++ apps/sudoku-web/src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 import packageJson from '../../package.json';
+import gitHash from './git-hash.json';
 import { Environment } from './environment.config';
 
 export const environment: Environment = {
   production: false,
-  version: `v${packageJson.version} - dev`,
+  version: `v${packageJson.version} - dev (${gitHash.hash})`,
 };
