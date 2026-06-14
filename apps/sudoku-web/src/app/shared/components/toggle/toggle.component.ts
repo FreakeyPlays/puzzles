6a45b6b7905a9ca29660e1db3b44047ae50807f6
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-06-14 11:39:50 +0200

    added toggle for settings page

---
+++ apps/sudoku-web/src/app/shared/components/toggle/toggle.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  imports: [],
+  templateUrl: './toggle.component.html',
+  styles: ``,
+})
+export class ToggleComponent {}
